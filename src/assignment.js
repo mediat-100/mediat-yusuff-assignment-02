da5ab535b8ce41c0d05ee9a86d929dcb4138810a
Mediat Yusuff <yusuftomiwa740@gmail.com>
2022-07-11 00:01:34 +0000

    attempt-1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0
+    for (let i of arrayOfNumbers) {
+      sum += i
+    }
+    return sum
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenArr = []
+    for (let i of arrayOfNumbers) {
+      if (i % 2 == 0) {
+        evenArr.push(i)
+      }
+    }
+    return evenArr.length
+    // return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
